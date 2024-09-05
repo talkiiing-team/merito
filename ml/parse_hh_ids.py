6648b914d767869bc326a5b535c2229310df11d6
iltya <iltyakov.nik@mail.ru>
2024-09-05 20:59:45 +0300

    add resume parser;add gitignore in ml; update reqs.txt

--- ml/parse_hh_ids.py
+++ ml/parse_hh_ids.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+import pickle
 import time
 
 driver_path = "chromedriver.exe"
@@ -29,7 +30,11 @@ def get_ids_by_request(search_link_base, num_pages=5):
 
         driver.get(link_for_search)
         # Даем странице время загрузиться
-        time.sleep(0.5)
+        time.sleep(1)
+
+        for i in range(5):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(0.2)
 
         # Получаем HTML-код страницы
         html = driver.page_source
@@ -37,7 +42,7 @@ def get_ids_by_request(search_link_base, num_pages=5):
 
         elements = soup.find_all('a', rel='nofollow')
         all_finded_links.extend([link['href'] for link in elements])
-
+        print(len(all_finded_links))
 
     # выдергиваем id-шники из ссылок
     all_finded_ids = [i.split("/")[2] for i in all_finded_links]
@@ -45,6 +50,12 @@ def get_ids_by_request(search_link_base, num_pages=5):
     return all_finded_ids
 
 if __name__ == "__main__":
-    all_finded_ids = get_ids_by_request(f"https://hh.ru/resumes/programmist?page=", num_pages=10)
-    print(all_finded_ids)
+    BASE_LINK = "https://hh.ru/search/resume?text=%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82&logic=normal&pos=full_text&exp_period=all_time&exp_company_size=any&filter_exp_period=all_time&area=113&relocation=living_or_relocation&age_from=&age_to=&gender=unknown&salary_from=&salary_to=&currency_code=RUR&order_by=relevance&search_period=0&items_on_page=100&hhtmFrom=resume_search_form&page="
+    all_finded_ids = get_ids_by_request(BASE_LINK, num_pages=100)
+    print("Len finded links list: ", len(all_finded_ids))
+
+    with open("parsed_links.pkl", "wb") as f:
+        pickle.dump(all_finded_ids, f)
+
+
 
---
+++ ml/resume_hh_parser.py
@@ -0,0 +1,26 @@
+import json
+import pickle
+from tqdm import tqdm
+import time
+
+from parse_hh_data import download, parse
+
+resume_ids = pickle.load(open("parsed_links.pkl", "rb"))
+resume_ids = [i for i in resume_ids if len(i) == 38]
+print("Finded resumes count: ", len(resume_ids))
+
+results = []
+
+for i, ids in enumerate(tqdm(resume_ids)):
+    if ids == "programmist":
+        continue
+    try:
+        a = parse.resume(download.resume(ids))
+        results.append(a)
+        time.sleep(1)
+    except:
+        continue
+
+    if i % 100 == 1 or i == len(resume_ids) - 1:
+        with open("programmist_hh_resumes.pkl", "wb") as f:
+            pickle.dump(results, f)
